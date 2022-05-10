862e24e1781ec60e197b0e0514ed19e9ba9b9ed0
Kenneth Bakke <kennethsbakke@gmail.com>
2022-05-10 16:58:22 -0700

    Set up particle chamber

--- languageAssessment/particleChamber.js
+++ languageAssessment/particleChamber.js
@@ -1,3 +1,39 @@
-function particleChamber() {
+/**
+ * We are writing a simulation of a particle chamber where individual particles
+ * are leaving the chamber at constant speed.
+ *
+ * Implement the method 'animate' that takes an initial position, and a speed
+ * that returns an array of strings that represents the number of positions that
+ * each particle travels in a given step.
+ *
+ * The input string will include either '.', 'L', or 'R'
+ *
+ * '.' represents an empty position
+ * 'R' represents a particle at that position that is traveling to the right
+ * 'L' represents a particle at that position that is traveling to the left
+ *
+ * Examples:
+ * Input ('..R....', 2)
+ * Output ['..X....', '....X..', '......X', '.......'];
+ *
+ * Input ('RR..LRL', 3)
+ * Output ['XX..XXX', '.X.XX..', 'X.....X', '.......']
+ *
+ * Input ('LRLR.LRLR', 2)
+ * Output ['XXXX.XXXX', 'X..X.X..X', '.X.....X.', '.........']
+ *
+ * Input('RLRLRLRLRL', 10)
+ * Output ['XXXXXXXXXX', '..........']
+ *
+ */
+
+/**
+ * @param {string} initialPosition
+ * @param {number} speed
+ * @return {string[]}
+ *
+ */
+
+function animate(initialPosition, speed) {
   // TODO
 }
