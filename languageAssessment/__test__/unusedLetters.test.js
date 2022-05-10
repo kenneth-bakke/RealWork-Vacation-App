e992249c6dc231e58384006184b09545ee50f076
Kenneth Bakke <kennethsbakke@gmail.com>
2022-05-10 16:45:50 -0700

    Unused Letters complete

---
+++ languageAssessment/__test__/unusedLetters.test.js
@@ -0,0 +1,24 @@
+const unusedLetters = require('../unusedLetters');
+
+describe('Unused letters', () => {
+  it('Returns a string', () => {
+    expect(typeof unusedLetters('This is a test string')).toBe('string');
+  });
+
+  it('Filters out the correct letters', () => {
+    let unused = unusedLetters(
+      'A slow yellow fox crawls under the proactive dog'
+    );
+    expect(unused).toBe('bjkmqz');
+  });
+
+  it('Returns an empty string when input string uses all letters', () => {
+    let unused = unusedLetters('A quick brown fox jumps over the lazy dog');
+    expect(unused).toBe('');
+  });
+
+  it('Returns an empty string when input string is empty', () => {
+    let unused = unusedLetters('');
+    expect(unused).toBe('');
+  });
+});
--- languageAssessment/unusedLetters.js
+++ languageAssessment/unusedLetters.js
@@ -1,3 +1,34 @@
-function unusedLetters() {
-  // TODO
+/**
+ * Given a string of English letters, write a function that
+ * returns all letters of the alphabet that are unused.
+ *
+ * For example
+ * Input: 'A slow yellow fox crawls under the proactive dog'
+ * Output: 'bjkmqz'
+ *
+ * All of the letters in the output can't be found in the input string
+ *
+ * Input: 'A quick brown fox jumps over the lazy dog'
+ * Output: ''
+ *
+ * All english letters are in the sentence
+ */
+
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function unusedLetters(string) {
+  if (!string) return '';
+
+  const missingLetters = [];
+  const stringCopy = string.toLowerCase();
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (stringCopy.indexOf(alphabet[i]) < 0) {
+      missingLetters.push(alphabet[i]);
+    }
+  }
+
+  return missingLetters.join('');
 }
+
+module.exports = unusedLetters;
